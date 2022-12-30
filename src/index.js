d4e5171c191060008181bcb34c986f1650386545
Zain Asif <zainclasher2002@gmail.com>
2022-12-30 23:28:56 +0500

    last-commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById("root")
-// )
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
