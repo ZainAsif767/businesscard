9c479d8bd0f854a9747d7c72321ebc84a02b7102
Zain Asif <zainclasher2002@gmail.com>
2022-12-30 15:33:21 +0500

    last-commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <div className="tags">
             <footer>
-                <a href="https://www.facebook.com/S.Zain.Asif/" className="fa fa-facebook"></a>
-                <a href="https://www.instagram.com/zain._.asif/" className="fa fa-instagram"></a>
-                <a href="https://github.com/ZainAsif767" className="fa fa-github"></a>
-                <a href="https://twitter.com/comeflywithme92" className="fa fa-twitter"></a>
+                <a href="https://www.facebook.com/S.Zain.Asif/" className="fa fa-facebook"></a >
+                <a href="https://www.instagram.com/zain._.asif/" className="fa fa-instagram"></a >
+                <a href="https://github.com/ZainAsif767" className="fa fa-github"></a >
+                <a href="https://twitter.com/comeflywithme92" className="fa fa-twitter"></ a>
             </footer>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById("root")
+// )
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
