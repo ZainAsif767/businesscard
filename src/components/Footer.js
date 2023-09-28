deee8056116c20d304a1f9362ac4e74e8987d295
ZainAsif767 <zainclasher2002@gmail.com>
2023-09-28 14:34:43 +0500

    aria-label for anchor tags

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 
 export default function Footer() {
     return (
         <div className="tags">
             <footer>
-                <a href="https://www.facebook.com/S.Zain.Asif/" className="fa fa-facebook"></a >
-                <a href="https://www.instagram.com/zain._.asif/" className="fa fa-instagram"></a >
-                <a href="https://github.com/ZainAsif767" className="fa fa-github"></a >
-                <a href="https://twitter.com/comeflywithme92" className="fa fa-twitter"></ a>
+                <a href="https://www.facebook.com/S.Zain.Asif/" className="fa fa-facebook" aria-label="Facebook"></a>
+                <a href="https://www.instagram.com/zain._.asif/" className="fa fa-instagram" aria-label="Instagram"></a>
+                <a href="https://github.com/ZainAsif767" className="fa fa-github" aria-label="Github"></a>
+                <a href="https://twitter.com/comeflywithme92" className="fa fa-twitter" aria-label="Twitter"></a>
             </footer>
         </div>
     )
